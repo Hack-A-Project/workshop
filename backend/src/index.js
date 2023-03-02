a017be3d92e6394d7c03688c7749ce3ab4f4f77a
andrewvt97 <andrewvt97@gmail.com>
2023-03-02 15:33:26 -0500

    Read in token and send back token plus one

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import bodyParser from "body-parser";
 
 const app = express();
 const PORT = 3000;
@@ -7,6 +8,18 @@ app.get('/', (req, res) => {
 	res.send('Hello World!');
 });
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.post('/api', (req, res) => {
+    let data = req.body;
+    let num = parseInt(data.token) + 1
+    let message = JSON.stringify({token: num});
+    res.send(message);
+	// res.send(`${num}`);
+});
 app.listen(PORT, () => {
 	console.log(`Example app listening on port ${PORT}`);
 });
+
