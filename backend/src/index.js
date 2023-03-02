a3ccf299ff73a1bb5960ecede850ffef0c36d7c2
prince-ao <prince.addo@cix.csi.cuny.edu>
2023-03-02 16:00:03 -0500

    backend website up

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,15 @@
 import express from 'express';
+import path from 'path';
 
 const app = express();
 const PORT = 3000;
 
+const __dirname = path.resolve(path.dirname(''));
+
 app.get('/', (req, res) => {
-	res.send('Hello World!');
+	res.sendFile(path.join(__dirname + '/frontend/weirdlinks.html'));
 });
 
 app.listen(PORT, () => {
-	console.log(`Example app listening on port ${PORT}`);
+	console.log(`app listening on port ${PORT}`);
 });
